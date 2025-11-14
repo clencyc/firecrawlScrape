ada1c652f0e21af1f824c0a2d5dc568aef729064
Clency Christine <clency2023@gmail.com>
2025-11-14 10:24:45 +0300

    Add new CORS allowed origins

--- main.py
+++ main.py
@@ -36,7 +36,9 @@ ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
     "https://haki-chain-liard.vercel.app",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "https://hakichain-blockchain-legal-institute.vercel.app/"
+    "https://hakichain-demo.vercel.app/"
 ]
 
 app.add_middleware(
